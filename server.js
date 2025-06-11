48688a4a1fdc95ce1b196c288000a56bf71381ea
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2025-06-11 16:04:20 +0530

    remove assert keyword usage

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
 import cartRouter from "./src/features/cartItems/cartItems.routes.js";
-import apiDocs from "./swagger.json" assert {type:'json'};
+import apiDocs from './swagger.json' with { type: 'json' };
 import orderRouter from "./src/features/order/order.routes.js";
 import likeRouter from "./src/features/like/like.routes.js";
 import { connectUsingMongoose } from "./src/config/mongooseConfig.js";
